3de504965061a4db9e799856353123aab6787c76
Vinícius Viana <viniciusvw18@hotmail.com>
2023-03-11 10:40:26 -0300

    Implementado função que gera lista aleatória
    ... para testar a ordenação com diversas listas diferentes.

--- semana03/exc07_insertion_sort.c
+++ semana03/exc07_insertion_sort.c
@@ -3,18 +3,26 @@
 // logo antes do próximo maior que ele.
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int *lista_aleatoria(int tamanho);
 int insertion_sort(int vetor[], int tamanho);
 void mostrar(int vetor[], int tamanho);
 
 int main(void)
 {
-    int vetor[] = {100, 1, 99, 101, 98, 2, 97, -3};
 
-    int tam = sizeof(vetor) / sizeof(int);
+    int tam = 10;
+    int *lista = lista_aleatoria(tam);
+    printf("lista aleatória: ");
+    mostrar(lista, 10);
 
-    insertion_sort(vetor, tam);
-    mostrar(vetor, tam);
+    insertion_sort(lista, tam);
+    printf("lista ordenada: ");
+    mostrar(lista, tam);
+
+    free(lista);
 }
 
 int insertion_sort(int vetor[], int tamanho)
@@ -31,7 +39,6 @@ int insertion_sort(int vetor[], int tamanho)
             vetor[j+1] = vetor[j];
             vetor[j] = atual;
             j--;     
-            mostrar(vetor, tamanho);
         }
         i++;    
     }
@@ -47,4 +54,15 @@ void mostrar(int vetor[], int tamanho)
         i++;
     }
     printf("]\n");
+}
+
+int *lista_aleatoria(int tamanho)
+{
+    srand(time(NULL));
+    int *lista = (int *)malloc(tamanho * sizeof(int));
+    for (int i = 0; i < tamanho; i++)
+    {
+        lista[i] = rand() % 100;
+    }
+    return lista;
 }
